bd73a3e85c882d563bc286bc7b4914171c8a486d
Nachu13 <65455361+Nachu13@users.noreply.github.com>
2020-05-20 10:46:43 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,11 +4,17 @@ var line1, line2, line3;
 function setup() {
   createCanvas(300,300);
   zenia = createSprite(50, 50, 10, 10);
-  tourus = createSprite(150, 50, 10, 10);
-  cyclap = createSprite(250, 50, 10, 10);
+  tourus = createSprite(50, 150, 10, 10);
+  cyclap = createSprite(50, 250, 10, 10);
+  line1= createSprite(200, 100, 350, 1);
+  line2= createSprite(200, 200, 350, 1);
+  line3= createSprite(200, 300, 350, 1);
+  wall1 = createSprite(50, 380, 10, 50);
+  wall2 = createSprite(150, 380, 10, 50);
+  wall3 = createSprite(250, 380, 10, 50);
 }
 
 function draw() {
-  background(255,255,255);  
+  background("#428f70");  
   drawSprites();
 }
